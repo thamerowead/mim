cddcb89306dd2257a09ae3bab40053232acc3ed6
thamerowead <thamerowead@gmail.com>
2026-06-09 14:47:23 +0300

    Update proline.js

--- api/proline.js
+++ api/proline.js
@@ -4,7 +4,7 @@
 
 const GH_OWNER  = 'thamerowead';
 const GH_REPO   = 'mim';
-const GH_BRANCH = 'master';
+const GH_BRANCH = 'main';
 const CACHE_PATH = 'data/projects-cache.json';
 
 const FETCH_TIMEOUT = 6000;
